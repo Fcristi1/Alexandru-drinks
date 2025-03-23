96f6a3751c1074e72fcd6603cf133b66a695691e
Fcristi1 <cristi.1q2@gmail.com>
2025-03-23 09:09:58 +0000

    3#

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// Add your JavaScript code here
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document is ready');
+    // You can add more JavaScript code here to enhance your website
+});
